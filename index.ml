2692ae535909b71e7d887737102a7f9bc053ea6c
Xavier Deschuyteneer <xavier.deschuyteneer@gmail.com>
2014-07-19 14:19:19 +0200

    drawing of circles and pen of spirograph

--- index.ml
+++ index.ml
@@ -34,14 +34,16 @@ let stroke_circle ctx x y radius =
   ctx##closePath ();
   ctx##stroke ();;
 
-let stroke_line ctx a b =
-  let x1, y1 = a
-  and x2, y2 = b in
+let stroke_line ctx x1 y1 x2 y2 =
   ctx##beginPath ();
   ctx##moveTo (x1, y1);
   ctx##lineTo (x2, y2);
   ctx##closePath ();
   ctx##stroke ();;
+
+let move_to ctx a =
+  let x, y = a in
+  ctx##moveTo (x, y);;
 
 let clear_canvas ctx x y w h=
   ctx##beginPath ();
@@ -49,22 +51,55 @@ let clear_canvas ctx x y w h=
   ctx##closePath ();
   ctx##stroke ();;
 
-let init_time = Unix.time ();;
+module Timer = struct
+  let timer = ref (fun _ -> 0.)
+  let init () = timer := (fun _ -> Unix.time ())
+  let make () = !timer ()
+  let get t = !timer () -. t
+end
+
+Timer.init ();;
+let initial_time = ref (Timer.make ());;
+
+let r1 = 90.;;
+let r2 = 30.;;
+let r3 = 50.;;
+let cx, cy = 200., 200.;;
+
+let inner_center cx cy teta =
+  let r = r1 -. r2 in
+  cx +. r *. cos teta, cy +. r *. sin teta;;
+
+let pen cx cy teta =
+  let teta = -. teta in
+  let r = r3 in
+  cx +. r *. cos teta, cy +. r *. sin teta;;
+
+let alpha = ref 0.;;
+
+let deg_of_rad teta = teta *. (200. /. pi);;
+let rad_of_deg teta = teta *. (pi /. 200.);;
 
 let rec loop ctx () =
   begin
-    debug "rendering";
-    let t = Unix.time () in
-    let ellapsed_time = (int_of_float (t -. init_time)) in
-    let factor = 10 * ellapsed_time in
-    if (100 + factor < 500) then
+    let timer = Timer.get !initial_time in
+    if (timer >= 1.) then
       begin
-        let a = (50 + factor, 50)
-        and b = (100 + factor, 100) in
+        debug "alpha: %f" !alpha;
+        alpha := !alpha +. (rad_of_deg 20.);
         clear_canvas ctx 0 0 500 500;
-        stroke_line ctx a b;
+        stroke_circle ctx cx cy r1;
+        (* let cx', cy' = cx +. r1 -. r2, cy in *)
+        let cx', cy' = inner_center cx cy !alpha in
+        stroke_circle ctx cx' cy' r2;
+        ctx##strokeStyle <- Js.string (create_color 255 0 0);
+        let a1, a2 = pen cx' cy' !alpha in
+        stroke_line ctx cx' cy' a1 a2;
+        (* stroke_circle ctx cx' cy' r3; *)
+        ctx##strokeStyle <- Js.string (create_color 0 0 0);
+        initial_time := Timer.make ();
       end;
-    Dom_html._requestAnimationFrame (Js.wrap_callback (loop ctx))
+    Dom_html._requestAnimationFrame (Js.wrap_callback (loop ctx));
   end;;
 
 Dom_html.window##onload <- Dom.handler (fun _ ->
@@ -72,7 +107,7 @@ Dom_html.window##onload <- Dom.handler (fun _ ->
                                           let color = create_color 0 0 0 in
                                           let canvas = create_canvas 500 500 in
                                           let ctx = get_context canvas in
-                                          ctx##lineWidth <- 10.;
+                                          ctx##lineWidth <- 1.;
                                           ctx##strokeStyle <- Js.string color;
                                           Firebug.console##log (Js.string "loaded-ocaml");
                                           Dom_html._requestAnimationFrame (Js.wrap_callback (loop ctx));
