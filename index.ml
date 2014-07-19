697bc4cdcdb299f8b579a995301ea18895633ef5
Xavier Deschuyteneer <xavier.deschuyteneer@gmail.com>
2014-07-19 11:28:20 +0200

    add basic render loop for canvas drawing

--- index.ml
+++ index.ml
@@ -2,6 +2,8 @@ open Lwt
 open Js
 module Html = Dom_html
 
+let pi = 4.0 *. atan 1.0;;
+
 let error f = Printf.ksprintf (fun s -> Firebug.console##error (Js.string s); failwith s) f
 let debug f = Printf.ksprintf (fun s -> Firebug.console##log(Js.string s)) f
 let alert f = Printf.ksprintf (fun s -> Dom_html.window##alert(Js.string s); failwith s) f
@@ -15,30 +17,64 @@ let create_canvas w h =
   c;;
 
 let create_color r g b =
-  CSS.Color.string_of_t (CSS.Color.rgb r g b)
+  CSS.Color.string_of_t (CSS.Color.rgb r g b);;
 
 let get_context canvas =
-  canvas##getContext (Html._2d_)
+  canvas##getContext (Html._2d_);;
 
-let stroke_line ?(color=(create_color 0 0 0)) ctx a b =
+let stroke_rect ctx x y w h =
+  ctx##beginPath ();
+  ctx##rect (x, y, w, h);
+  ctx##closePath ();
+  ctx##stroke ();;
+
+let stroke_circle ctx x y radius =
+  ctx##beginPath ();
+  ctx##arc (x, y, radius, 0, (2. *. pi), Js._true);
+  ctx##closePath ();
+  ctx##stroke ();;
+
+let stroke_line ctx a b =
   let x1, y1 = a
   and x2, y2 = b in
-  ctx##lineWidth <- 10.;
-  ctx##strokeStyle <- Js.string color;
   ctx##beginPath ();
   ctx##moveTo (x1, y1);
   ctx##lineTo (x2, y2);
   ctx##closePath ();
   ctx##stroke ();;
 
+let clear_canvas ctx x y w h=
+  ctx##beginPath ();
+  ctx##clearRect (x, y, w, h);
+  ctx##closePath ();
+  ctx##stroke ();;
+
+let init_time = Unix.time ();;
+
+let rec loop ctx () =
+  begin
+    debug "rendering";
+    let t = Unix.time () in
+    let ellapsed_time = (int_of_float (t -. init_time)) in
+    let factor = 10 * ellapsed_time in
+    if (100 + factor < 500) then
+      begin
+        let a = (50 + factor, 50)
+        and b = (100 + factor, 100) in
+        clear_canvas ctx 0 0 500 500;
+        stroke_line ctx a b;
+      end;
+    Dom_html._requestAnimationFrame (Js.wrap_callback (loop ctx))
+  end;;
+
 Dom_html.window##onload <- Dom.handler (fun _ ->
                                         begin
-                                          let color = create_color 255 0 0 in
+                                          let color = create_color 0 0 0 in
                                           let canvas = create_canvas 500 500 in
                                           let ctx = get_context canvas in
-                                          let a = (50, 50)
-                                          and b = (100, 100) in
-                                          stroke_line ctx a b;
+                                          ctx##lineWidth <- 10.;
+                                          ctx##strokeStyle <- Js.string color;
                                           Firebug.console##log (Js.string "loaded-ocaml");
+                                          Dom_html._requestAnimationFrame (Js.wrap_callback (loop ctx));
                                           Js._true
-                                        end)
+                                        end);;
